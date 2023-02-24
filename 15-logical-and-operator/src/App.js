6253706618a53df330347370a71672e479f44e04
Vladi Khan <84037014+vladikhan@users.noreply.github.com>
2023-02-25 00:24:16 +0900

    Add files via upload

---
+++ 15-logical-and-operator/src/App.js
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import './App.css'
+import Counter from './components/Counter'
+import Button from './components/Button'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const incrementCount = () => {
+    setCount(count + 1)
+  }
+  const resetCount = () => {
+    setCount(0)
+  }
+
+  const buttonStyle = { backgroundColor: 'pink' }
+
+  return (
+    <div className="App">
+      <Counter count={count} />
+      <Button onClick={incrementCount} />
+      <Button onClick={incrementCount} />
+      <Button onClick={incrementCount} />
+      <Button onClick={incrementCount} />
+      {count > 0 && (
+        <div>
+          <button style={buttonStyle} onClick={resetCount}>
+            Reset
+          </button>
+        </div>
+      )}
+    </div>
+  )
+}
+export default App
---
+++ 15-logical-and-operator/src/components/Button.js
@@ -0,0 +1,4 @@
+function Button({ onClick }) {
+  return <button onClick={onClick}>Click me</button>
+}
+export default Button
